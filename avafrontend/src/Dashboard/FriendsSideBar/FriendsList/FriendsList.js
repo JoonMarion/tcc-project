24d594060f1a09d4322c5c1cee4d2bedfda21dd6
JoonMariano <marianojoao52@gmail.com>
2023-05-30 10:47:49 -0300

    feat: friendlist online indicator

--- avafrontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
+++ avafrontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { styled } from '@mui/system';
+import FriendListItem from './FriendListItem';
+
+const DUMMY_FRIENDS = [
+    {
+        id: '1',
+        username: 'John Doe',
+        isOnline: true,
+    },
+    {
+        id: '2',
+        username: 'Jane Doe',
+        isOnline: false,
+    },
+    {
+        id: '3',
+        username: 'John Smith',
+        isOnline: true,
+    },
+];
 
 const MainContainer = styled('div')({
-  flexGrow: 1,
-  width: '100%',
-})
+    flexGrow: 1,
+    width: '100%',
+});
 
 const FriendsList = () => {
-  return (
-    <MainContainer></MainContainer>
-  )
-}
+    return (
+        <MainContainer>
+            {DUMMY_FRIENDS.map((f) => (
+                <FriendListItem username={f.username} id={f.id} key={f.id} isOnline={f.isOnline} />
+            ))}
+        </MainContainer>
+    );
+};
 
-export default FriendsList
+export default FriendsList;
