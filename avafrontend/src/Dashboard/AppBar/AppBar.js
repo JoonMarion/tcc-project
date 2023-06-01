aa4459d29da59a1bcbfe94b1e0a996a4d968c7fd
JoonMariano <marianojoao52@gmail.com>
2023-06-01 13:55:27 -0300

    feat: dropdown menu logout

--- avafrontend/src/Dashboard/AppBar/AppBar.js
+++ avafrontend/src/Dashboard/AppBar/AppBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { styled } from '@mui/system';
+import DrowpdownMenu from './DropdownMenu';
 
 const MainContainer = styled('div')({
     position: 'absolute',
@@ -16,7 +17,11 @@ const MainContainer = styled('div')({
 });
 
 const AppBar = () => {
-    return <MainContainer></MainContainer>;
+    return (
+        <MainContainer>
+            <DrowpdownMenu />
+        </MainContainer>
+    );
 };
 
 export default AppBar;
